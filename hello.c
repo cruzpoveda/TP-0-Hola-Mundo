d3d537b27fb10b3b916a78e62a2afcae312690c5
cruzpoveda <cruzpoveda@yahoo.com>
2015-04-05 15:41:05 -0300

    Update and rename Hola Mundo Poveda to hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* Programa Hola Mundo, muestra por pantalla el mensaje 'Hola JC Poveda'
+* Juan Cruz Poveda
+* 3/4/15
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hola JC Poveda";
+}
